871a6c5575d164f889cc503705db6cd505004b06
Bernardo Dornelles <bernardo.dornelles22@gmail.com>
2022-05-30 16:25:33 -0300

    atualizando

---
+++ Algoritmos e programação/exercicio2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+int n1,n2,n3,media;
+printf ("digite o primeiro valor\n");
+scanf ("%d%*c", &n1);
+printf ("digite o segundo valor\n");
+scanf ("%d%*c", &n2);
+printf ("digite o terceiro valor\n");
+scanf ("%d%*c", &n3);
+printf ("A media de %.1d e %.1d e %.1d = %.1d" ,n1, n2, n3, (n1+n2+n3)/3 );
+
+return 0;
+}
---
+++ Algoritmos e programação/exercicio3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main (void) {
+int n1, n2, n3, p1, p2, p3, s1, s2, media;
+printf ("digite a primeira nota\n");
+scanf ("%d*c", &n1);
+printf ("digite a segunda nota\n");
+scanf ("%d%*c", &n2);
+printf ("digite a terceira nota\n");
+scanf ("%d%*c", &n3);
+printf ("digite o peso1 \n");
+scanf ("%d%*c", &p1);
+printf ("digite o peso2 \n");
+scanf ("%d%*c", &p2);
+printf ("digite o peso3 \n");
+scanf ("%d%*c", &p3);
+s1=(n1*p1)+(n2*p2)+(n3*p3);
+s2=(p1+p2+p3);
+media= (s1/s2);
+printf ("A media e: %d", media);
+return 0;
+}
