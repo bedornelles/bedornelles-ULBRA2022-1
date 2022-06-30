e5b1bf49677e04ceb0db696519812db383b8c13a
Bernardo Dornelles <bernardo.dornelles22@gmail.com>
2022-06-30 16:34:39 -0300

    atualização

---
+++ Algoritmos e programação/Atividades C+/Tde 1-10/exercicio10.c
@@ -0,0 +1,32 @@
+//Faça um programa que calcule e mostre a área de um círculo. Sabe-se que: Área = p * R2.
+
+#include <stdio.h>
+#include <locale.h>
+#include <windows.h>
+
+int main () {
+    float area, raio, total;
+
+    printf ("Digite o valor do raio:\n");
+    scanf ("%f*%c", &raio);
+
+    area= (raio * raio) * 3,14;
+
+
+
+    printf ("A area da figura é: %.4f\n", area);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
